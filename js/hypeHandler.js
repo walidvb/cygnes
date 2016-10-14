28d38e69f2210a3ad35dff645e628965da081823
Walid <walidvb@gmail.com>
2016-10-14 14:16:09 +0200

    removes all assets

--- js/hypeHandler.js
+++ js/hypeHandler.js
@@ -1,10 +1,5 @@
-function Hyper(){
-  function getHTML(choice){
-    return $('<div id="'+ choice +'_hype_container" style="margin:auto;position:relative;width:100vw;height:100vh;overflow:hidden;" aria-live="polite"> \
-    <script type="text/javascript" charset="utf-8" src="'+choice+'.hyperesources/'+choice+'_hype_generated_script.js"></script> \
-    </div>');
-  }
-
+function Hyper(hyperDocument){
+  
   function play(choice){
     $('body').append(getHTML(choice));
   }
