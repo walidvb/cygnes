ba209fd1ccb7e4781512f6018779e1b7cf906321
Walid <walidvb@gmail.com>
2016-10-20 10:42:27 +0200

    moves to documents

--- sketcher.sh
+++ sketcher.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-cd ~/Desktop/cygnes;
+cd ~/Documents/cygnes;
 git pull -f;
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --kiosk --app=http://localhost:8080/sketcher.html >/dev/null &disown;
 php -S localhost:8080 >/dev/null &disown;
